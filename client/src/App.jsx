64040669bb92ff55b409db8c3f848eb5771370d9
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-16 01:44:08 +0700

    create a component ui footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,27 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'  
-import Home from './pages/Home'
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import DashBoard from './pages/DashBoard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from './components/Footer';
 
 
-
-export default function App () {
+export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/about' element={<About/>}/>
-        <Route path='/sign-in' element={<SignIn />}/>
-        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
