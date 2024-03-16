bba857587190ef3180e06be4b02e63a914a5e11e
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-17 02:30:26 +0700

    add posts section to the dashboard

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 function DashBoard() {
 
@@ -23,7 +24,9 @@ function DashBoard() {
         <DashSidebar/>
       </div>
         {/* profile */}
-        {tab == 'profile' && <DashProfile/>}
+        {tab === 'profile' && <DashProfile/>}
+        {/* posts */}
+        {tab === 'posts' && <DashPosts/>}
     </div>
   )
 }
