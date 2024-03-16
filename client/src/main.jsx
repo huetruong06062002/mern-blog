f47f3801a28535295839227e5d8c0ed441f8025d
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-16 10:50:16 +0700

    fix bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,8 @@ import App from './App.jsx'
 import './index.css'
 
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
 import {  PersistGate } from 'redux-persist/integration/react'
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
