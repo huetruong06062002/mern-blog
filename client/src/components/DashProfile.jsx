3c58cd0328a806dcac901c34921505ef4a8ede7b
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-16 13:04:15 +0700

    complete sidebar of dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+
+function DashProfile() {
+
+
+
+  return (
+    <div>
+      DashProfile
+    </div>
+  )
+}
+
+export default DashProfile
