8333c4dd2650143efcecd5486dfce1df7938c946
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-17 22:27:34 +0700

    complet post page functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -141,7 +141,11 @@ export default function UpdatePost() {
             <option value='uncategorized'>Select a category</option>
             <option value='javascript'>JavaScript</option>
             <option value='reactjs'>React.js</option>
+            <option value='nodejs'>Node.js</option>
+            <option value='expressjs'>Express.js</option>
             <option value='nextjs'>Next.js</option>
+            <option value='nestjs'>Nest.js</option>
+
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
