24849770edfc5fa0a1ec9a2a5f147cd59381a088
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-17 11:53:20 +0700

    show user posts inside dashboard

--- api/model/post.modal.js
+++ api/model/post.modal.js
@@ -15,7 +15,7 @@ const postSchema = new mongoose.Schema({
     },
     image: {
       type:String,
-      default:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwordtracker-swoop-uploads.s3.amazonaws.com%2Fuploads%2Fckeditor%2Fpictures%2F1247%2Fcontent_wordtracker_blog_article_image.jpg&tbnid=SmZv80V4ysWrKM&vet=12ahUKEwiGyr6jofmEAxXzoa8BHZRfDe0QMygaegUIARCFAQ..i&imgrefurl=https%3A%2F%2Fwww.wordtracker.com%2Fblog%2Fcontent%2F25-ways-to-promote-your-blog-post&docid=zN6irx2FnFDupM&w=640&h=550&q=blog%20post%20image&ved=2ahUKEwiGyr6jofmEAxXzoa8BHZRfDe0QMygaegUIARCFAQ',
+      default:'https://wordtracker-swoop-uploads.s3.amazonaws.com/uploads/ckeditor/pictures/1247/content_wordtracker_blog_article_image.jpg',
     },
     category: {
       type: String,
