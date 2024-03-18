48cfaea1766996fab32e18613711f5768680a9af
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-19 02:01:21 +0700

    add dashboard overview  to the admin dashboard

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,41 +1,42 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom'
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
-import DashPosts from '../components/DashPosts';
-import DashUsers from '../components/DashUsers';
-import DashComments from '../components/DashComment';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComment";
+import DashboardComp from '../components/DashboardComp';
 
 function DashBoard() {
-
   const location = useLocation();
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState("");
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
-    if(tabFromUrl){
-      setTab(tabFromUrl)
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
-  }, [location.search])
+  }, [location.search]);
 
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='md:w-56'>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
         {/* sidebar */}
-        <DashSidebar/>
+        <DashSidebar />
       </div>
-        {/* profile */}
-        {tab === 'profile' && <DashProfile/>}
-        {/* posts */}
-        {tab === 'posts' && <DashPosts/>}
-        {/* user */}
-        {tab === 'users' && <DashUsers/>}
-        {/* comments */}
-        {tab === 'comments' && <DashComments/>}
-
+      {/* profile */}
+      {tab === "profile" && <DashProfile />}
+      {/* posts */}
+      {tab === "posts" && <DashPosts />}
+      {/* user */}
+      {tab === "users" && <DashUsers />}
+      {/* comments */}
+      {tab === "comments" && <DashComments />}
+      {/* dashboard */}
+      {tab === "dash" && <DashboardComp />}
     </div>
-  )
+  );
 }
 
-export default DashBoard
+export default DashBoard;
