973af6bcdfda7299d90e4a4a670ec5765ecd4ab9
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-18 13:32:35 +0700

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,27 @@
+import { Button } from 'flowbite-react'
+
+function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 items-center rouded-tl-3xl rounded br-3xl text-center">
+      <div className='flex-1 justify-center flex flex-col'>
+        <h2 className='text-2xl'>
+          Want to learn more about Javascript?
+        </h2>
+        <p className='text-gray-500 my-2'>
+          Checkout these resources with Javscript algorithms
+        </p>
+        <Button gradientDuoTone="purpleToPink" className='rounded-tl-xl rouded-bl-none'>
+          <a href='https://github.com/anonystick/anonystick/tree/main/Library/javascript' target="_blank" 
+            rel="noopener noreferrer">
+            Javscript algorithms  
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img src="https://cloud.z.com/vn/wp-content/uploads/2024/01/Screenshot_1-6.png" />
+      </div>
+    </div>
+  )
+}
+
+export default CallToAction
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button } from 'flowbite-react';
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -56,6 +57,9 @@ export default function PostPage() {
       </div>
       <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+      </div>
+      <div className="max-w-4l mx-auto w-full">
+        <CallToAction/>
       </div>
     </main>
   );
