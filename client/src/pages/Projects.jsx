c621808f6384617d18e71ab74dc84b8bfa9686a9
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-03-19 11:51:49 +0700

    fix some problems

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
+import CallToAction from '../components/CallToAction'
+
 function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1>Projects</h1>
+      <p className='text-md text-gray-500'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, adipisci esse. Molestiae quia consequuntur et eius illo, inventore alias soluta pariatur accusamus voluptate necessitatibus ipsa ab suscipit officia sint velit!
+      </p>
+      <p className='text-md text-gray-500'></p>
+      <CallToAction/>
     </div>
   )
 }
